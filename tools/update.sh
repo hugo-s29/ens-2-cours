2010755fb3a247a996d8997538c9c8c257aa879b
Hugo Salou <y.hugo.s29@gmail.com>
2025-09-08 13:46:22 +0200

    Added CS[CM] and CAP[CM]

--- tools/update.sh
+++ tools/update.sh
@@ -10,14 +10,23 @@ if [ "$HOSTNAME" = "s-1vcpu-1gb-lon1-01" ]; then
   # cp all-m1.pdf ../web/public/data/ens1/all.pdf
 
   # ==========
-  # SEMESTRE 2
-  # ==========
-
-
-  # ==========
   # SEMESTRE 1
   # ==========
 
-  echo "It's compile time!"
+  #cp pdap/main.pdf   ../web/public/data/ens2/pdap.pdf
+  #cp pdap/chap00.pdf ../web/public/data/ens2/pdap-chap00.pdf
+  #cp pdap/chap01.pdf ../web/public/data/ens2/pdap-chap01.pdf
 
+  cp cs/main.pdf   ../web/public/data/ens2/cs.pdf
+  cp cs/chap00.pdf ../web/public/data/ens2/cs-chap00.pdf
+  cp cs/chap01.pdf ../web/public/data/ens2/cs-chap01.pdf
+
+  cp cap/main.pdf   ../web/public/data/ens2/cap.pdf
+  cp cap/chap00.pdf ../web/public/data/ens2/cap-chap00.pdf
+  #cp cap/chap01.pdf ../web/public/data/ens2/cap-chap01.pdf
+
+
+  # ==========
+  # SEMESTRE 2
+  # ==========
 fi
