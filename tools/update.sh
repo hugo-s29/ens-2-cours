d70ebb33b0ad66235e1845d627aa1d183a972d01
Hugo Salou <y.hugo.s29@gmail.com>
2025-09-08 20:14:07 +0200

    Added SV[CM]

--- tools/update.sh
+++ tools/update.sh
@@ -13,10 +13,6 @@ if [ "$HOSTNAME" = "s-1vcpu-1gb-lon1-01" ]; then
   # SEMESTRE 1
   # ==========
 
-  #cp pdap/main.pdf   ../web/public/data/ens2/pdap.pdf
-  #cp pdap/chap00.pdf ../web/public/data/ens2/pdap-chap00.pdf
-  #cp pdap/chap01.pdf ../web/public/data/ens2/pdap-chap01.pdf
-
   cp cs/main.pdf   ../web/public/data/ens2/cs.pdf
   cp cs/chap00.pdf ../web/public/data/ens2/cs-chap00.pdf
   cp cs/chap01.pdf ../web/public/data/ens2/cs-chap01.pdf
@@ -24,6 +20,10 @@ if [ "$HOSTNAME" = "s-1vcpu-1gb-lon1-01" ]; then
   cp cap/main.pdf   ../web/public/data/ens2/cap.pdf
   cp cap/chap00.pdf ../web/public/data/ens2/cap-chap00.pdf
   #cp cap/chap01.pdf ../web/public/data/ens2/cap-chap01.pdf
+
+  cp sv/main.pdf   ../web/public/data/ens2/sv.pdf
+  cp sv/chap00.pdf ../web/public/data/ens2/sv-chap00.pdf
+  cp sv/chap01.pdf ../web/public/data/ens2/sv-chap01.pdf
 
 
   # ==========
