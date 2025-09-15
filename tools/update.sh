a80434f4794b2a7e1b4d614c8ec8066832317786
Hugo Salou <y.hugo.s29@gmail.com>
2025-09-15 18:56:31 +0200

    Added TDs to the update script

--- tools/update.sh
+++ tools/update.sh
@@ -13,6 +13,7 @@ if [ "$HOSTNAME" = "s-1vcpu-1gb-lon1-01" ]; then
   # SEMESTRE 1
   # ==========
 
+  cp cs/td.pdf     ../web/public/data/ens2/cs-td.pdf
   cp cs/main.pdf   ../web/public/data/ens2/cs.pdf
   cp cs/chap00.pdf ../web/public/data/ens2/cs-chap00.pdf
   cp cs/chap01.pdf ../web/public/data/ens2/cs-chap01.pdf
@@ -20,13 +21,16 @@ if [ "$HOSTNAME" = "s-1vcpu-1gb-lon1-01" ]; then
   cp cap/main.pdf   ../web/public/data/ens2/cap.pdf
   cp cap/chap00.pdf ../web/public/data/ens2/cap-chap00.pdf
 
+  cp sv/td.pdf     ../web/public/data/ens2/sv-td.pdf
   cp sv/main.pdf   ../web/public/data/ens2/sv.pdf
   cp sv/chap00.pdf ../web/public/data/ens2/sv-chap00.pdf
   cp sv/chap01.pdf ../web/public/data/ens2/sv-chap01.pdf
 
+  cp opt/td.pdf     ../web/public/data/ens2/opt-td.pdf
   cp opt/main.pdf   ../web/public/data/ens2/opt.pdf
   cp opt/chap00.pdf ../web/public/data/ens2/opt-chap00.pdf
 
+  cp qcs/td.pdf     ../web/public/data/ens2/qcs-td.pdf
   cp qcs/main.pdf   ../web/public/data/ens2/qcs.pdf
   cp qcs/chap00.pdf ../web/public/data/ens2/qcs-chap00.pdf
   cp qcs/chap01.pdf ../web/public/data/ens2/qcs-chap01.pdf
